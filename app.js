ae4e18351bc837ebf90081c2f47e2be0b46f5039
Kelvin <kelvin.z.pan@gmail.com>
2017-09-17 14:06:08 -0700

    Express, EJS part

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var http = require('http');
+var express = require('express');
 
-var server = http.createServer(function(req, res){
-  console.log('Request was made: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('Now listening to port 3000.');
+app.get('/contact', function(req, res) {
+	res.send('Contact Me');
+});
+
+app.get('/profile/:name', function(req, res){
+	var data = {age: 18, job: 'student', hobbies: ['gaming', 'programming', 'chillin']};
+	res.render('profile', {person: req.params.name, data: data});
+});
+
+app.listen(3000);
